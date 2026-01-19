26c1469689bd5fdafab0fbfe94dfba93c326ab28
hxnnzs <hannes-kirchner@gmx.de>
2026-01-19 11:44:51 +0100

    Bug Fix

--- src/components/LogoMenu/LogoMenu.jsx
+++ src/components/LogoMenu/LogoMenu.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 import { 
     exportDocument, 
     importDocument, 
